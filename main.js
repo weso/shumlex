d7eb2cf271fb63581f0a539ff2912a7ffe512b31
fidalgoLXXVI <UO258524@uniovi.es>
2022-03-22 11:20:57 +0100

    0.6.4

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const shexparser = require('./src/shex_util/ShExParser.js');
 const xmiparser = require('./src/xmi_util/XMIParser.js');
-const GrafoGen = require("./src/visual/GrafoGen.js");
 const UMLGen = require("./src/visual/UMLGen.js");
-let grgen = new GrafoGen();
 let umlgen = new UMLGen();
 
 function shExToXMI(text) {
@@ -12,10 +10,6 @@ function shExToXMI(text) {
 
 function XMIToShEx(text) {
     return xmiparser.parseXMIToShEx(text);
-}
-
-function crearGrafo(shex) {
-	return grgen.shExAGrafo(shex);
 }
 
 function crearMUML(xmi) {
@@ -44,7 +38,6 @@ function base64SVG(idsvg) {
 module.exports = {
     shExToXMI,
     XMIToShEx,
-	crearGrafo,
 	crearMUML,
 	crearDiagramaUML,
 	base64SVG,
